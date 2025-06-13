d66e3ffb4c920416d9cde87c9ab7ff9a98c56d25
Ondřej Tůma <ondrej.tuma.024@pslib.cz>
2025-06-14 00:26:18 +0200

    Update dicegame.py
    More weapons, critical hits, dice sides, descriptions

--- dicegame.py
+++ dicegame.py
@@ -11,13 +11,15 @@ class Enemy():
         return f"ENEMY | LVL: {enemy.level} | HP: {enemy.hp} | DMG: {enemy.dmg}"
 
 class Weapon():
-    def __init__(self, name, price, dmgMulti):
+    def __init__(self, name, price, dmgMulti, sides, desc):
         self.name = name
         self.price = price
         self.dmgMulti = dmgMulti
+        self.sides = sides
+        self.desc = desc
 
     def __str__(self):
-        return f"{self.name} | Price: {self.price} | Damage Multiplier: {self.dmgMulti}"
+        return f"{self.name} | Price: {self.price} | {self.desc}"
 
 class Item():
     def __init__(self, name, price, desc):
@@ -28,11 +30,20 @@ class Item():
     def __str__(self):
         return f"{self.name} | Price: {self.price} | {self.desc}"
 
+def crit(chance):
+    if random.randint(1, 100) <= chance:
+        print("CRITICAL HIT!")
+        return roll * weapon.dmgMulti * 2
+    else:
+        return roll * weapon.dmgMulti
+
 
 # WEAPONS
-dice = Weapon("Dice", 0, 1)
-goldenDice = Weapon("Golden Dice", 100, 2)
-gamblerDice = Weapon("Gambler's Dice", 250, 3)
+dice = Weapon("Dice", 0, 1, 6, "The starting weapon.")
+betterDice = Weapon("Better Dice", 100, 2, 6, "Double the damage.")
+gamblerDice = Weapon("Gambler's Dice", 250, 2, 6, "Double the damage and has a 25% chance to crit.")
+goldenDice = Weapon("Golden Dice", 1000, 4, 6, "4x damage.")
+twelveSideDice = Weapon("12-Sided Dice", 2500, 3, 12, "3x damage and rolls up to 12.")
 
 # ITEMS
 bandage = Item("Bandage", 50, "Heals 25 HP.")
@@ -40,11 +51,11 @@ bandage = Item("Bandage", 50, "Heals 25 HP.")
 # STATS
 coins = 100
 hp = 100
-inventory = [goldenDice, bandage]
-weapon = dice
+inventory = [bandage]
+weapon = twelveSideDice
 
 
-shop = [goldenDice, gamblerDice, bandage]
+shop = [betterDice, goldenDice, gamblerDice, twelveSideDice, bandage]
 
 
 while True:
@@ -59,9 +70,12 @@ while True:
             print(f"YOU | HP: {hp}")
             choice = input("ROLL/FLEE: ").lower()
             if choice == "roll":
-                roll = random.randint(1, 6)
-                attack = roll * weapon.dmgMulti
                 print("-------------------------------------------")
+                roll = random.randint(1, weapon.sides)
+                if weapon == gamblerDice:
+                    attack = crit(25)
+                else:
+                    attack = roll * weapon.dmgMulti
                 print(f"You rolled a {roll} and did {attack} damage!")
                 enemy.hp -= attack
                 hp -= enemy.dmg
@@ -106,7 +120,7 @@ while True:
             print("-------------------------------------------")
             print(f"HP: {hp}")
             print(f"Coins: {coins}")
-            print(f"Weapon: {weapon.name} | Price: {weapon.price} | Damage Multiplier: {weapon.dmgMulti}")
+            print(f"Weapon: {weapon}")
             print("Inventory:")
             temp = 0
             for x in inventory:
